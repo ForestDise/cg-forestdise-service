400afec7495a1eab17fbc158dcd60ad4ed819b73
sQuocKhanhs <innespham@gmail.com>
2023-09-27 09:49:46 +0700

    first commit

---
+++ src/main/java/com/forestdise/ForestDiseApplication.java
@@ -0,0 +1,13 @@
+package com.forestdise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestDiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForestDiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/forestdise/ForestDiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forestdise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForestDiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
