3bbbb3f73ca0aeef5ff722ef0242e0ad9ba70c61
DESKTOP-QM2UL00\Admin <pphp1998@gmail.com>
2023-10-02 16:18:51 +0700

    CF-8-Add script for product detail

--- src/main/java/com/forestdise/service/IProductService.java
+++ src/main/java/com/forestdise/service/IProductService.java
@@ -1,7 +1,12 @@
 package com.forestdise.service;
 
 import com.forestdise.dto.ProductDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface IProductService {
     public ProductDto getProductById(Long id);
+    public List<ProductDto> getAllProductDtos();
 }
--- src/main/java/com/forestdise/service/impl/ProductService.java
+++ src/main/java/com/forestdise/service/impl/ProductService.java
@@ -6,7 +6,12 @@ import com.forestdise.entity.Product;
 import com.forestdise.repository.ProductRepository;
 import com.forestdise.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService implements IProductService {
@@ -17,6 +22,11 @@ public class ProductService implements IProductService {
     public ProductDto getProductById(Long id) {
         Product product = productRepository.findById(id).orElse(null) ;
         return productConverter.entityToDTO(product);
+    }
+
+    @Override
+    public List<ProductDto> getAllProductDtos() {
+        return productConverter.entitiesToDTOs(productRepository.findAll());
     }
 
 }
