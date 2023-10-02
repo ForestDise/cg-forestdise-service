3ae9eb5ea5bc4745147d42e116ab1335b672471c
DESKTOP-QM2UL00\Admin <pphp1998@gmail.com>
2023-10-02 16:40:20 +0700

    CF-8-Add script for product detail

--- src/main/resources/db/migration/V1_11__InsertData.sql
+++ src/main/resources/db/migration/V1_11__InsertData.sql
@@ -1,10 +1,6 @@
 INSERT IGNORE INTO category (attribute) VALUES ('Category 1');
 INSERT IGNORE INTO category (attribute) VALUES ('Category 2');
-INSERT IGNORE INTO product (title, description, main_picture, status, create_at, updated_at, category_id)
-VALUES ('Product 1', 'Description 1', 'picture1.jpg', 'Active', NOW(), NOW(), 1);
 
-INSERT IGNORE INTO product (title, description, main_picture, status, create_at, updated_at, category_id)
-VALUES ('Product 2', 'Description 2', 'picture2.jpg', 'Active', NOW(), NOW(), 2);
 INSERT IGNORE INTO variant (name, sku_code, stock_quantity, weight, price, img, product_id)
 VALUES ('Variant 1', 'SKU001', 100, 0.5, 20.99, 'variant1.jpg', 1);
 
