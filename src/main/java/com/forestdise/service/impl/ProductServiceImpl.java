b3cdb48796f3f81f533d19c50d877673a905493d
DESKTOP-QM2UL00\Admin <pphp1998@gmail.com>
2023-10-06 15:17:03 +0700

    CF-8 fix

--- src/main/java/com/forestdise/service/impl/ProductServiceImpl.java
+++ src/main/java/com/forestdise/service/impl/ProductServiceImpl.java
@@ -26,4 +26,6 @@ public class ProductServiceImpl implements IProductService {
         return productConverterImpl.entitiesToDTOs(productRepository.findAll());
     }
 
+
+
 }
--- src/main/resources/db/migration/V1_09__Create_VariantOption_Table.sql
+++ src/main/resources/db/migration/V1_09__Create_VariantOption_Table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS VARIANT_OPTIONVALUE (
+                                ID INT AUTO_INCREMENT PRIMARY KEY,
                                 VARIANT_ID INT,
                                 OPTIONVALUE_ID INT,
-                                PRIMARY KEY (VARIANT_ID, OPTIONVALUE_ID),
+--                                 PRIMARY KEY (VARIANT_ID, OPTIONVALUE_ID),
                                 FOREIGN KEY (VARIANT_ID) REFERENCES VARIANT(ID),
                                 FOREIGN KEY (OPTIONVALUE_ID) REFERENCES OPTIONVALUE(ID)
 );
