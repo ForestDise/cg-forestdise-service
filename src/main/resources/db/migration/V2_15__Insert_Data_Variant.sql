c31725406ab4d0d3f01187eeab937e0ae6b75554
sQuocKhanhs <126664421+sQKhanhs@users.noreply.github.com>
2023-10-29 17:55:33 +0700

    Update V2_15__Insert_Data_Variant.sql

--- src/main/resources/db/migration/V2_15__Insert_Data_Variant.sql
+++ src/main/resources/db/migration/V2_15__Insert_Data_Variant.sql
@@ -1,5 +1,5 @@
 INSERT IGNORE INTO VARIANT (name, sku_code, stock_quantity, weight, price,sale_price, img, product_id)
-VALUES ('Samsung 1 49-Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA) – Super Ultrawide Screen QLED', 'SKU002', 50, 0.7, 17.91,10.19, 'https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg', 1);
+VALUES ('SAMSUNG Galaxy Z Flip 5 Cell Phone', 'SKU002', 50, 0.7, 17.91,10.19, 'https://m.media-amazon.com/images/I/61NKaFRAyRL._AC_SL1500_.jpg', 1);
 INSERT IGNORE INTO VARIANT (name, sku_code, stock_quantity, weight, price,sale_price, img, product_id)
 VALUES ('Kissonic 2 mens 2 Piece Outfits Casual Cotton Linen Shirt and High Waisted Drawstring Shorts Lounge Tracksuit Sets Blue M', 'SKU002', 50, 0.7, 24.99,20.99, 'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg', 1);
 INSERT IGNORE INTO VARIANT (name, sku_code, stock_quantity, weight, price,sale_price, img, product_id)
@@ -7,7 +7,7 @@ VALUES ('Samsung 3 49-Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA) �
 INSERT IGNORE INTO VARIANT (name, sku_code, stock_quantity, weight, price,sale_price, img, product_id)
 VALUES ('Kissonic 4 mens 2 Piece Outfits Casual Cotton Linen Shirt and High Waisted Drawstring Shorts Lounge Tracksuit Sets Blue M', 'SKU002', 50, 0.7, 24.99,20.99, 'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg', 1);
 INSERT IGNORE INTO VARIANT (name, sku_code, stock_quantity, weight, price,sale_price, img, product_id)
-VALUES ('Samsung 1 49-Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA) – Super Ultrawide Screen QLED', 'SKU002', 50, 0.7, 27.91,10.19, 'https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg', 2);
+VALUES ('SAMSUNG Galaxy S23+ Plus Cell Phone', 'SKU002', 50, 0.7, 27.91,10.19, 'https://m.media-amazon.com/images/I/71wmNVsS9TL._AC_SL1500_.jpg', 2);
 INSERT IGNORE INTO VARIANT (name, sku_code, stock_quantity, weight, price,sale_price, img, product_id)
 VALUES ('Kissonic 2 mens 2 Piece Outfits Casual Cotton Linen Shirt and High Waisted Drawstring Shorts Lounge Tracksuit Sets Blue M', 'SKU002', 50, 0.7, 24.99,20.99, 'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg', 2);
 INSERT IGNORE INTO VARIANT (name, sku_code, stock_quantity, weight, price,sale_price, img, product_id)
