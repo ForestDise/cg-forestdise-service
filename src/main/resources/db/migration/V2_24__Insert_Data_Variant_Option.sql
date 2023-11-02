5498ad2bc322cec704033344bfb3a403d09ba0cd
Hoangnghiango <ngothoangnghia999@gmail.com>
2023-11-02 11:35:40 +0700

    CV- EDIT API VARIANT CREATE

--- src/main/resources/db/migration/V2_24__Insert_Data_Variant_Option.sql
+++
@@ -1,4 +0,0 @@
-INSERT IGNORE INTO VARIANT_OPTIONVALUE (id, variant_id, optionvalue_id)
-VALUES (1, 1, 1);
-INSERT IGNORE INTO VARIANT_OPTIONVALUE (id, variant_id, optionvalue_id)
-VALUES (2, 2, 2);
