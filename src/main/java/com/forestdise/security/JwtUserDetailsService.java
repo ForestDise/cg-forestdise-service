27958851d04ea35d8c5ba48215f6de83f097f203
sQuocKhanhs <innespham@gmail.com>
2023-11-03 08:36:13 +0700

    Update email sender

--- src/main/java/com/forestdise/security/JwtUserDetailsService.java
+++ src/main/java/com/forestdise/security/JwtUserDetailsService.java
@@ -3,8 +3,12 @@ package com.forestdise.security;
 import com.forestdise.constraint.Role;
 import com.forestdise.entity.Seller;
 import com.forestdise.entity.User;
+import com.forestdise.entity.VerificationToken;
 import com.forestdise.repository.SellerRepository;
 import com.forestdise.repository.UserRepository;
+import com.forestdise.repository.VerificationTokenRepository;
+import com.forestdise.service.EmailService;
+import com.forestdise.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -13,6 +17,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
@@ -20,17 +25,38 @@ import java.util.stream.Collectors;
 public class JwtUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
-
     @Autowired
     private SellerRepository sellerRepository;
+    @Autowired
+    private VerificationTokenRepository verificationTokenRepository;
+    @Autowired
+    private EmailService emailService;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email);
 
         if (user != null) {
-            throw new UsernameNotFoundException("Email has already been registered");
-        }else{
+            VerificationToken token = verificationTokenRepository.findByUser(user);
+            if(user.isEnabled()){
+                throw new UsernameNotFoundException("Email has already been registered");
+            } else{
+                boolean isExpired;
+                Calendar cal = Calendar.getInstance();
+                if ((token.getExpiryDate().getTime() - cal.getTime().getTime()) <= 0) {
+                    isExpired = true;
+                } else{
+                    isExpired = false;
+                }
+                String tokenUID = token.getToken();
+                if(isExpired){
+                    emailService.sendConfirmEmail(user, tokenUID);
+                    return null;
+                } else {
+                    throw new UsernameNotFoundException("Email has not been enabled");
+                }
+            }
+        }else {
                 return null;
         }
     }
